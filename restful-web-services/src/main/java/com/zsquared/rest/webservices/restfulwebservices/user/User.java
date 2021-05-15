0175a6727d5a508358fab828b263e297ca5f6fc9
Randy <RandyZeleznak@gmail.com>
2021-05-15 15:31:26 -0500

    Tutorial#47 - Security issue - 051521:1530

--- restful-web-services/src/main/java/com/zsquared/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/zsquared/rest/webservices/restfulwebservices/user/User.java
@@ -5,14 +5,20 @@ import java.util.Date;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Details about the User")
 public class User {
 	
 	private Integer id;
 	
 	@Size(min=2, message = "Must include at least two characters")
+	@ApiModelProperty(notes="Name Must include at least two characters ")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="Birthdate should be in the past")
 	private Date birthDate;
 	
 	protected User() {
---
+++ restful-web-services/src/main/java/com/zsquared/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,49 @@
+package com.zsquared.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+	
+	@GetMapping("v1/person")
+	public PersonV1 personv1() {
+		return new PersonV1("Bob Charles");
+	}
+
+	@GetMapping("v2/person")
+	public PersonV2 personv2() {
+		return new PersonV2(new Name("Bob","Charles"));
+	}
+	
+	@GetMapping(value = "/person/param", params="version=1")
+	public PersonV1 paramv1() {
+		return new PersonV1("Bob Charles");
+	}
+
+	@GetMapping(value = "/person/param", params="version=2")
+	public PersonV2 paramv2() {
+		return new PersonV2(new Name("Bob","Charles"));
+	}
+	
+	@GetMapping(value = "/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerv1() {
+		return new PersonV1("Bob Charles");
+	}
+
+	@GetMapping(value = "/person/header", headers="X-API-VERSION=2")
+	public PersonV2 headerv2() {
+		return new PersonV2(new Name("Bob","Charles"));
+	}
+	
+	@GetMapping(value = "/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 producesv1() {
+		return new PersonV1("Bob Charles");
+	}
+
+	@GetMapping(value = "/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 producesv2() {
+		return new PersonV2(new Name("Bob","Charles"));
+	}
+	
+}
