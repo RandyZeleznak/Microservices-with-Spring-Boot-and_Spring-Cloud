2176bd4c5697233d59e92f991154d8c4cb621cd6
Randy <RandyZeleznak@gmail.com>
2021-06-25 07:49:15 -0500

    localhost8888 server error? - 062621:0745

---
+++ currency-exchange-service/src/test/java/com/zsquared/microservices/currencyexchangedemo/CurrencyExchangeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsquared.microservices.currencyexchangedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
